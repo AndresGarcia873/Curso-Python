aa034975f4abc352c16d94a596715aeb35e91ffd
AndresGarcia <andresgarcia@worldofficeonline.com>
2021-10-22 11:49:57 -0500

    Se agrega código clase 05

---
+++ Clase 05. Comentarios/Clase 05. Comentarios.py
@@ -0,0 +1,26 @@
+#Esto es un comentario
+print("Hola")
+
+"Esto es otro comentario"
+print("Hola 2")
+
+'Esto es otro comentario'
+print("Hola 3")
+
+"""
+Esto
+es
+un
+comentario
+multilinea"""
+
+print("Hola 4")
+
+'''
+Esto
+es
+otro
+comentario
+multilinea
+'''
+print("Hola 5")

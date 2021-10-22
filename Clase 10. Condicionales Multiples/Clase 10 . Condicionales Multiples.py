7bcf3db0cd8c924ef564b99b30b77fdec4e1496e
AndresGarcia <andresgarcia@worldofficeonline.com>
2021-10-22 12:16:35 -0500

    Se agrega código clase 10

---
+++ Clase 10. Condicionales Multiples/Clase 10 . Condicionales Multiples.py
@@ -0,0 +1,37 @@
+"""num_uno = 1;
+
+if num_uno == 1 :
+    print("El número es UNO.");
+elif num_uno == 2 :
+    print("El número es DOS.");
+else :
+    print("El número se desconoce.");"""
+print("=============================");
+print("!Convertidor de números a letras!");
+print("=============================");
+
+num_uno = int(input("¿Cúal es el número que deseas convertir?: "));
+
+if num_uno == 1 :
+    print("El número es 'Uno'.");
+elif num_uno == 2 :
+    print("El número es 'Dos'.");
+elif num_uno == 3 :
+    print("El número es 'Tres'.");
+elif num_uno == 4 :
+    print("El número es 'Cuatro'.");
+elif num_uno == 5 :
+    print("El número es 'Cinco'.");
+elif num_uno == 6 :
+    print("El número es 'Seis'.");
+elif num_uno == 7 :
+    print("El número es 'Siete'.");
+elif num_uno == 8 :
+    print("El número es 'Ocho'.");
+elif num_uno == 9 :
+    print("El número es 'Nueve'.");
+elif num_uno == 10 :
+    print("El número es 'Diez'.");
+else :
+    print("Este programa solo puede convertir hasta el número 10.");
+    
